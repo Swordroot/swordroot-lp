b8ade1ba1b21c79e59e4929169f4ddadd0478a90
刀根諒 <tstarouimnpkegto@gmail.com>
2019-07-16 00:57:13 +0900

    環境構築、LPプロトタイプ作成

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import NuxtConfiguration from '@nuxt/config'
+
+const config: NuxtConfiguration = {
+  // Type or Press `Ctrl + Space` for autocompletion
+  mode: 'spa',
+
+  head: {
+    titleTemplate: 'Swordroot\'s portfolio site',
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      { name: "viewport", content: "width=device-width, initial-scale=1.0" }
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+      }
+    ]
+  }
+}
+
+export default config
---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <section class="hero is-medium is-dark is-bold">
+      <div class="hero-body">
+        <h1 class="title">Swordroot's Portfolio</h1>
+      </div>
+    </section>
+    <section class="section">
+      <div class="tile is-ancestor">
+        <div class="tile is-3 is-child">
+          <figure class="image is-3by4">
+            <img src="~/assets/portfolio.jpg">
+          </figure>
+        </div>
+        <div class="tile is-9 is-vertical is-parent box">
+          <p class="title is-1">About me</p>
+          <div class="tile is-parent">
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Name</span></p>
+              <p>刀根 諒（Makoto Tone）</p>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Age</span></p>
+              <p>25</p>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Working for...</span></p>
+              <p>Aidemy.inc</p>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Twitter</span></p>
+              <p>@Swordroot_M</p>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Qiita</span></p>
+              <p>@Swordroot_M</p>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Github</span></p>
+              <p>@swordroot</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="tile is-ancestor">
+        <div class="tile is-12 is-vertical is-parent box">
+          <p class="title is-1">Skills</p>
+          <div class="tile is-parent">
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Typescript</span></p>
+              <p style="text-align: right">80%</p>
+              <progress class="progress" value="80" max="100">80%</progress>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Typescript</span></p>
+              <p style="text-align: right">80%</p>
+              <progress class="progress" value="80" max="100">80%</progress>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Typescript</span></p>
+              <p style="text-align: right">80%</p>
+              <progress class="progress" value="80" max="100">80%</progress>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Typescript</span></p>
+              <p style="text-align: right">80%</p>
+              <progress class="progress" value="80" max="100">80%</progress>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Typescript</span></p>
+              <p style="text-align: right">80%</p>
+              <progress class="progress" value="80" max="100">80%</progress>
+            </div>
+            <div class="tile is-child is-4 column">
+              <p><span class="subtitle is-3">Typescript</span></p>
+              <p style="text-align: right">80%</p>
+              <progress class="progress" value="80" max="100">80%</progress>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
