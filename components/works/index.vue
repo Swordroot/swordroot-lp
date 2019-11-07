0f64afee0c843694285f3741728fbb28eed938ef
Makoto Tone <tstarouimnpkegto@gmail.com>
2019-11-07 18:34:44 +0900

    成果物の部分を実装

--- components/works/index.vue
+++ components/works/index.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="container">aaa{{$data.text}}</div>
+  <section class="section">
+    <div class="tile is-ancestor">
+      <div class="tile is-12 is-vertical is-parent box">
+        <p class="title is-1">Works</p>
+        <div class="tile is-parent" v-for="row in works" v-bind:key="row.id">
+          <cell
+            v-for="col in row.cells"
+            v-bind:key="col.id"
+            :title="col.title"
+            :link="col.link"
+            :description="col.description"
+            :ogp_title="col.ogp.title"
+            :ogp_image_src="col.ogp.image"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script lang="ts">
 import Vue from "vue";
-import axios from "axios";
+import cell from "./components/works_cell.vue";
 export default Vue.extend({
-  async asyncData() {
-    console.log("yeah");
-    const resp = await axios.get("https://aidemy.net/courses/4100");
-    console.log(resp);
-    return {
-      text: resp
-    };
+  props: {
+    works: Array
+  },
+  components: {
+    cell
   }
 });
 </script>
